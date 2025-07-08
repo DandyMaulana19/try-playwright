2e6ed44e3cf6cb972ee0f8dd2523c0f1f885340e
DandyMaulana19 <dandyainul19@gmail.com>
2025-07-08 15:20:04 +0700

    test(add): adding invalid login test case

--- tests/learn.spec.ts
+++ tests/learn.spec.ts
@@ -48,3 +48,82 @@ test.describe("TC-LGN-001", { tag: "@Valid Login" }, () => {
     });
   });
 });
+
+test.describe("TC-LGN-002", { tag: "@Invalid Login" }, () => {
+  test.describe(
+    "Login with Unverified Email",
+    { tag: "@Unverified Email Login" },
+    () => {
+      test("As a user I see an alert when logging in with an unverified email", async ({
+        page,
+      }) => {
+        await page.goto("http://localhost:8000");
+        await page.getByRole("button", { name: "" }).click();
+        await page.locator('[href="/auth/login"]').click();
+        await expect(page).toHaveTitle("Login | Mesti Minum");
+
+        const userEmail = process.env.UNVERIFIED_USER_EMAIL as string;
+        const userPassword = process.env.USER_PASSWORD as string;
+
+        await page.getByLabel("Email address").fill(userEmail);
+        await page.getByLabel("Password").fill(userPassword);
+        await page.getByRole("button", { name: "login" }).click();
+
+        await expect(
+          page.getByText(/Email anda belum di verifikasi/i)
+        ).toBeVisible();
+      });
+    }
+  );
+
+  test.describe(
+    "Login with wrong Email more than 3 times",
+    { tag: "@Load Balancer Login" },
+    () => {
+      test("As a user I see an alert when logging in with a wrong email more than 3 times", async ({
+        page,
+      }) => {
+        await page.goto("http://localhost:8000");
+        await page.getByRole("button", { name: "" }).click();
+        await page.locator('[href="/auth/login"]').click();
+        await expect(page).toHaveTitle("Login | Mesti Minum");
+
+        const userEmail = process.env.UNACTIVE_USER_EMAIL as string;
+
+        for (let index = 1; index <= 3; index++) {
+          await page.getByLabel("Email address").fill(userEmail);
+          await page.getByLabel("Password").fill("userPassword");
+          await page.getByRole("button", { name: "login" }).click();
+        }
+
+        await expect(
+          page.getByText(/Silahkan tunggu \d+ detik lagi untuk login kembali/i)
+        ).toBeVisible();
+      });
+    }
+  );
+  test.describe(
+    "Login with wrong Email/Password",
+    { tag: "@Wrong Email/Password Login" },
+    () => {
+      test("As a user I see an alert when logging in with a wrong email/password", async ({
+        page,
+      }) => {
+        await page.goto("http://localhost:8000");
+        await page.getByRole("button", { name: "" }).click();
+        await page.locator('[href="/auth/login"]').click();
+        await expect(page).toHaveTitle("Login | Mesti Minum");
+
+        const userEmail = process.env.UNACTIVE_USER_EMAIL as string;
+
+        await page.getByLabel("Email address").fill(userEmail);
+        await page.getByLabel("Password").fill("userPassword");
+        await page.getByRole("button", { name: "login" }).click();
+
+        await expect(
+          page.getByText(/Email atau Password Salah!/i)
+        ).toBeVisible();
+      });
+    }
+  );
+});
